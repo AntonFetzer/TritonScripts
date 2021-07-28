cd061b6a9ff1f60c94938df7626f2a96d7782d03
Anton Fetzer <anton.fetzer@aalto.fi>
2021-07-28 14:18:59 +0300

    3DFullAnalysis seems to work now

--- 3DFullAnalysis.py
+++ 3DFullAnalysis.py
@@ -3,11 +3,14 @@ from ReadMultipleRoot import readMultipleRoot
 from MeVtokRad_3D import MeVtokRad_3D
 
 Path = "/home/anton/Desktop/triton_work/3D/SiChipTestModular/root/"
-File = "1mmal3dmultichip1e8solarproton10mev.root"
+File = "1mmal3dmultichip2e9electrons500kev.root"
+NORM_FACTOR_SPECTRUM = 5.886798E+14
+Npart = 2e9
+Radius = 10  # cm
 
 GunEnergy, Edep, Esec = readMultipleRoot(Path + File)
 
-GunEnergyMeV = sum(GunEnergy)
+GunEnergyTotalMeV = sum(GunEnergy)
 
 NumberVolumes = len(Edep)
 
@@ -18,7 +21,7 @@ for i in range(NumberVolumes):
     EdepTotalMeV.append(sum(Edep[i]))
     EsecTotalMeV.append(sum(Esec[i]))
 
-print("GunEnergyMeV:", GunEnergyMeV)
+print("GunEnergyTotalMeV:", GunEnergyTotalMeV)
 print("EdepTotalMeV:", EdepTotalMeV[0])
 print("EsecTotalMeV:", EsecTotalMeV[0])
 
@@ -35,8 +38,36 @@ for i in range(100):
     for j in range(NumberVolumes):
         EdepSamples[j][i] = sum(Edep[j][SampleLen * i:SampleLen * (i + 1) - 1])
         EsecSamples[j][i] = sum(Esec[j][SampleLen * i:SampleLen * (i + 1) - 1])
-
     # print("From:", SampleLen*i)
     # print("To:", SampleLen*(i+1)-1)
 
-print(GunEnergy / EdepTotalMeV[0])
+GunEnergyStdMeV = np.std(GunEnergySamples)
+EdepStdMeV = []
+EsecStdMeV = []
+
+for j in range(NumberVolumes):
+    EdepStdMeV.append(np.std(EdepSamples[j][:]))
+    EsecStdMeV.append(np.std(EsecSamples[j][:]))
+
+print("GunEnergyStdMeV %:", 100 * GunEnergyStdMeV / GunEnergyTotalMeV)
+print("EdepStdMeV %:", 100 * EdepStdMeV[0] / EdepTotalMeV[0])
+print("EsecStdMeV %:", 100 * EsecStdMeV[0] / EsecTotalMeV[0])
+
+GunEnergyTotalKRAD = MeVtokRad_3D(GunEnergyTotalMeV, NORM_FACTOR_SPECTRUM, Npart, Radius)
+GunEnergyStdKRAD = MeVtokRad_3D(GunEnergyStdMeV, NORM_FACTOR_SPECTRUM, Npart, Radius)
+
+EdepTotalKRAD = []
+EsecTotalKRAD = []
+EdepStdKRAD = []
+EsecStdKRAD = []
+
+for i in range(NumberVolumes):
+    EdepTotalKRAD.append(MeVtokRad_3D(EdepTotalMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+    EsecTotalKRAD.append(MeVtokRad_3D(EsecTotalMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+    EdepStdKRAD.append(MeVtokRad_3D(EdepStdMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+    EsecStdKRAD.append(MeVtokRad_3D(EsecStdMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+
+
+print("GunEnergyTotalKRAD:", GunEnergyTotalKRAD)
+for i in range(NumberVolumes):
+    print("EdepTotalKRAD", str(i), EdepTotalKRAD[i], "+-", EdepStdKRAD[i])
--- MeVtokRad_3D.py
+++ MeVtokRad_3D.py
@@ -1,7 +1,7 @@
 import math
 
 
-def MeVtokRad_3D(MeV, NORM_FACTOR_SPECTRUM, Npart):
+def MeVtokRad_3D(MeV, NORM_FACTOR_SPECTRUM, Npart, Radius):  # Radius in cm
 
     # ----------- Norm factors from GPS spectrum ------------
     NORM_FACTOR_ANGULAR = 2.500000E-01
@@ -9,7 +9,6 @@ def MeVtokRad_3D(MeV, NORM_FACTOR_SPECTRUM, Npart):
     # -----------------------------------------
 
     # --------- Simulated Particle Fluence --------
-    Radius = 10  # cm **************************************************************************************************
     Area = 4 * math.pi * Radius * Radius  # cm2
     SimulatedFluence = Npart / Area
     # ------------------------------------
@@ -18,7 +17,7 @@ def MeVtokRad_3D(MeV, NORM_FACTOR_SPECTRUM, Npart):
     MissionMeV = MeV / SimulatedFluence * Norm
     # -------------------------------
 
-    print("MissionMeV:", MissionMeV)  # MeV
+    # print("MissionMeV:", MissionMeV)  # MeV
 
     # ------ Sensitive Volume ------
     SiDensity = 2.33 * 1e-3  # kg/cm3
@@ -26,20 +25,20 @@ def MeVtokRad_3D(MeV, NORM_FACTOR_SPECTRUM, Npart):
     SiLength = 0.3  # cm ***********************************************************************************************
     SiWidth = 0.3  # cm ************************************************************************************************
     SiVol = SiThick * SiLength * SiWidth
-    print("SiVol", SiVol)  # cm3
+    # print("SiVol", SiVol)  # cm3
     SiMass = SiVol * SiDensity
-    print("SiMass", SiMass)  # kg
+    # print("SiMass", SiMass)  # kg
     # -----------------------------
 
     JperMev = 1.602E-13
     MissionJoule = MissionMeV * JperMev
-    print("MissionJoule", MissionJoule)  # J
+    # print("MissionJoule", MissionJoule)  # J
 
     Grays = MissionJoule / SiMass
-    print("Grays", Grays)  # J/kg
+    # print("Grays", Grays)  # J/kg
 
     kRads = Grays / 10
-    print("kRads", kRads)  # krad
+    # print("kRads", kRads)  # krad
 
     return kRads
 
