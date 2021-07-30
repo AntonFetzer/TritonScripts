8e5b0e869951a3b7a3a94f6fc47882328587d6f0
Anton Fetzer <anton.fetzer@aalto.fi>
2021-07-30 16:20:00 +0300

    3DFullAnalysis write to CSV added

--- 3DFullAnalysis.py
+++ 3DFullAnalysis.py
@@ -2,57 +2,76 @@ import numpy as np
 from ReadMultipleRoot import readMultipleRoot
 from MeVtokRad_3D import MeVtokRad_3D
 import matplotlib.pyplot as plt
+from natsort import natsorted
+import os
 
-Path = "/home/anton/Desktop/triton_work/3D/MultiChipTest/root/Electron/"
-File = "1-mmal3dmultichip2e9electrons500kev.root"
-NORM_FACTOR_SPECTRUM = 5.886798E+14
+Path = "/home/anton/Desktop/triton_work/3D/MultiChipTest/root/SolarProton/"
+# NORM_FACTOR_SPECTRUM = 5.886798E+14  # Electron500keV
+# NORM_FACTOR_SPECTRUM = 3.381390E+11  # Protons10MeV
+NORM_FACTOR_SPECTRUM = 1.109681E+10  # SolarProton10MeV
+
 Npart = 2e9
 Radius = 10  # cm
 
-Data = readMultipleRoot(
-    Path + File)  # ['Sivol_0_Edep_MeV', ... , 'Sivol_0_Esec_MeV', ... , 'Gun_energy_MeV', 'Gun_angle_deg']
+# Get list of all root files in that folder
+Files = [f for f in os.listdir(Path) if f.endswith('.root')]
+Files = natsorted(Files)
 
-NumDataSets = len(Data)
-
+NumDataSets = 0
 TotalMeV = []
-
-for i in range(NumDataSets):
-    TotalMeV.append(sum(Data[i]))
-
-print("Edep1TotalMeV:", TotalMeV[0])
-
-# -------------------------- Standard Deviation ---------------------------
-NumPoints = len(Data[0])
-SampleLen = int(NumPoints / 100)
-
-Samples = np.zeros(100)
 StdMeV = []
-
-for j in range(NumDataSets):
-    for i in range(100):
-        Samples[i] = sum(Data[j][SampleLen * i:SampleLen * (i + 1) - 1])
-    # print("From:", SampleLen*i)
-    # print("To:", SampleLen*(i+1)-1)
-    # print("Samples:", Samples)
-    StdMeV.append(np.std(Samples))
-
-print("Edep1StdMeV:", StdMeV[0])
-
-# --------- Conversion ##############
 TotalKRAD = []
 StdKRAD = []
 
-for i in range(NumDataSets):
-    TotalKRAD.append(MeVtokRad_3D(TotalMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
-    StdKRAD.append(MeVtokRad_3D(StdMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+CSVfile = open(Path + Files[0].split(".")[0].split("-")[1] + "DoseTable.txt", 'w')
 
-print("Edep1TotalKRAD:", TotalKRAD[0])
-print("Edep1StdKRAD:", StdKRAD[0])
+for File in Files:
 
-print("Edep1StdMeV %:", 100 * TotalKRAD[0] / StdKRAD[0])
-print("Esec1StdMeV %:", 100 * TotalKRAD[4] / StdKRAD[4])
+    Data = readMultipleRoot(
+        Path + File)  # ['Sivol_0_Edep_MeV', ... , 'Sivol_0_Esec_MeV', ... , 'Gun_energy_MeV', 'Gun_angle_deg']
 
-print("GunEnergyTotalKRAD:", TotalKRAD[-2])
-for i in range(4):
-    print("EdepTotalKRAD", str(i), TotalKRAD[i], "+-", TotalKRAD[i])
-    print("EsecTotalKRAD", str(i), TotalKRAD[i + 4], "+-", TotalKRAD[i + 4])
+    NumDataSets = len(Data)
+    NumSivols = int(NumDataSets / 2) - 1
+
+    TotalMeV = []
+
+    for i in range(NumDataSets):
+        TotalMeV.append(sum(Data[i]))
+
+    # -------------------------- Standard Deviation ---------------------------
+    NumPoints = len(Data[0])
+    SampleLen = int(NumPoints / 100)
+
+    Samples = np.zeros(100)
+    StdMeV = []
+
+    for j in range(NumDataSets):
+        for i in range(100):
+            Samples[i] = sum(Data[j][SampleLen * i:SampleLen * (i + 1) - 1])
+        # print("From:", SampleLen*i)
+        # print("To:", SampleLen*(i+1)-1)
+        # print("Samples:", Samples)
+        StdMeV.append(np.std(Samples))
+
+    # --------- Conversion ##############
+    TotalKRAD = []
+    StdKRAD = []
+
+    for i in range(NumDataSets):
+        TotalKRAD.append(MeVtokRad_3D(TotalMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+        StdKRAD.append(MeVtokRad_3D(StdMeV[i], NORM_FACTOR_SPECTRUM, Npart, Radius))
+
+    print("GunEnergyTotalKRAD:", TotalKRAD[-2], "+-", 100 * StdKRAD[-2] / TotalKRAD[-2])
+    for i in range(NumSivols):
+        print("EdepTotalKRAD", str(i), TotalKRAD[i], "+-", 100 * StdKRAD[i] / TotalKRAD[i])
+        print("EsecTotalKRAD", str(i), TotalKRAD[i + 4], "+-", 100 * StdKRAD[i + 4] / TotalKRAD[i + 4])
+
+    # ---------- Write to CSV File ----------------------------------
+
+    CSVfile.write(File + ", Dose Table in kRad, Number of Points: " + str(NumPoints) + "\n")
+    CSVfile.write(File.split("-")[0] + "mm Al Shielding Thickness, Edep, EdepStd, Esec, EsecStd\n")
+    for i in range(NumSivols):
+        CSVfile.write(','.join(["Sivol_" + str(i), str(TotalKRAD[i]), str(StdKRAD[i]), str(TotalKRAD[i + NumSivols]), str(StdKRAD[i + NumSivols]) + "\n"]))
+
+    CSVfile.write("\n")
+CSVfile.close()
